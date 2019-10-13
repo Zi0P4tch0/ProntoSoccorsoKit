2ddac6169d21cf141af7a61688ce33ae091f960b
Matteo Pacini <ispeakprogramming@gmail.com>
2019-10-13 12:55:15 +0200

    Conversion to dynamic framework

---
+++ ProntoSoccorsoKit/ProntoSoccorsoKit.h
@@ -0,0 +1,19 @@
+//
+//  ProntoSoccorsoKit.h
+//  ProntoSoccorsoKit
+//
+//  Created by Matteo Pacini on 13/10/2019.
+//  Copyright © 2019 Matteo Pacini. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ProntoSoccorsoKit.
+FOUNDATION_EXPORT double ProntoSoccorsoKitVersionNumber;
+
+//! Project version string for ProntoSoccorsoKit.
+FOUNDATION_EXPORT const unsigned char ProntoSoccorsoKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProntoSoccorsoKit/PublicHeader.h>
+
+
